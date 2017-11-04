b6ef793be652a781b9837102281800e9a04b5281
w1cked91 <giunta.nicolas@gmail.com>
2017-11-04 14:08:37 +0100

    Update Convert-Handbrake.ps1
    Fix variable assigment indentation

--- Convert-Handbrake.ps1
+++ Convert-Handbrake.ps1
@@ -6,24 +6,24 @@ $username     = "w1cked"
 $password     = "@Dm!Nnico8991x"
 
 function FileToMp4 ($sourceFile, $targetMp4, $handbrakeFolder) {
-    $cmd = "$handbrakeFolder\HandBrakeCLI.exe"
-    $presetSwitch = "-Z"
-    $presetValue = "Fast 1080p30"
-    #$presetValue = "HQ 1080p30 Surround"
+    $cmd           = "$handbrakeFolder\HandBrakeCLI.exe"
+    $presetSwitch  = "-Z"
+    $presetValue   = "Fast 1080p30"
+    #$presetValue   = "HQ 1080p30 Surround"
     $verboseSwitch = "--verbose=1"
     $sourceFile
     & $cmd $presetSwitch $presetValue -i $sourceFile -o $targetMp4 $verboseSwitch
 }
 
 function Copy-SourceTimeStampToTarget ($sourceFile, $targetMp4) {
-    $srcTime = Get-Item  $sourceFile
-    $targetTime = Get-Item $targetMp4
+    $srcTime                  = Get-Item  $sourceFile
+    $targetTime               = Get-Item $targetMp4
     $targetTime.LastWriteTime = $srcTime.LastWriteTime
-    $targetTime.CreationTime = $srcTime.CreationTime
+    $targetTime.CreationTime  = $srcTime.CreationTime
 }
 
 function Get-FileTypeCount ($folder, $extension) {
-    $set = (Get-ChildItem $folder -Filter "*.$extension") | Measure-Object
+    $set      = (Get-ChildItem $folder -Filter "*.$extension") | Measure-Object
     $setCount = $set.Count
     "In folder [$folder], there are [$setCount] files of type [$extension]"
     start-sleep 2
@@ -40,9 +40,9 @@ function Remove-FileNameFromFullPath ($file) {
 
 function Get-VideoDuration ($fullPath) {
     $LengthColumn = 27
-    $objShell = New-Object -ComObject Shell.Application 
-    $objFolder = $objShell.Namespace($(Remove-FileNameFromFullPath $fullPath))
-    $objFile = $objFolder.ParseName($(Get-FileNameFromFullPath $fullPath))
+    $objShell     = New-Object -ComObject Shell.Application 
+    $objFolder    = $objShell.Namespace($(Remove-FileNameFromFullPath $fullPath))
+    $objFile      = $objFolder.ParseName($(Get-FileNameFromFullPath $fullPath))
     $objFolder.GetDetailsOf($objFile, $LengthColumn)
 }
 
@@ -50,24 +50,24 @@ function Convert-FromFileToMp4File ($sourceFile, $mp4target, $handbrakeFolder) {
     $source
     $target
     start-sleep 2
-    $startTime = Get-Date
+    $startTime        = Get-Date
     FileToMp4 -sourceFile $source -targetMp4 $target -h $handbrakeFolder
-    $endTime = Get-Date
+    $endTime          = Get-Date
     $secondsToConvert = Round-Number(($endTime - $startTime).TotalSeconds)
     Get-Date | Out-File -Append $logFile
     "Completed conversion of [$source] in [$secondsToConvert] seconds" | Out-File -Append $logFile 
     
-    $sourceSizeInMb = Round-Number((Get-Item -Path $source).Length/1MB)
-    $targetSizeInMb = Round-Number((Get-Item -Path $target).Length/1MB)
+    $sourceSizeInMb   = Round-Number((Get-Item -Path $source).Length/1MB)
+    $targetSizeInMb   = Round-Number((Get-Item -Path $target).Length/1MB)
     "sourceSizeInMb: [$sourceSizeInMb]" | Out-File -Append $logFile 
     "targetSizeInMb: [$targetSizeInMb]" | Out-File -Append $logFile 
 
-    $conversionRate =  Round-Number($sourceSizeInMb/$secondsToConvert)
+    $conversionRate   =  Round-Number($sourceSizeInMb/$secondsToConvert)
     "Conversion rate was [$conversionRate]MB per second" | Out-File -Append $logFile 
     $compressionRatio =  Round-Number($sourceSizeInMb/$targetSizeInMb)
     "Compression ratio was $compressionRatio ([$sourceSizeInMb]/[$targetSizeInMb])" | Out-File -Append $logFile 
-    $fileDuration =  Get-VideoDuration -fullPath $source
-    $mp4Duration =  Get-VideoDuration -fullPath $target
+    $fileDuration     =  Get-VideoDuration -fullPath $source
+    $mp4Duration      =  Get-VideoDuration -fullPath $target
     "File duration: [$fileDuration]" | Out-File -Append $logFile 
     "MP4 duration: [$mp4Duration] (these should match)" | Out-File -Append $logFile 
 
